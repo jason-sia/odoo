e0dc84c3b8e81ed848c2295b78da4b5bf5ed74d0
Jason Sia <jasonbsia@gmail.com>
2024-05-23 09:01:56 +0800

    Update install-odoo16-on-ubuntu20.04LTS.sh

--- install-odoo16-on-ubuntu20.04LTS.sh
+++ install-odoo16-on-ubuntu20.04LTS.sh
@@ -64,6 +64,8 @@ sudo runuser -l odoo16 -c 'source /opt/odoo16/odoo-venv/bin/activate'
 sudo runuser -l odoo16 -c 'pip3 install wheel'
 sudo runuser -l odoo16 -c 'cd /'
 sudo runuser -l odoo16 -c 'pip3 install -r /opt/odoo16/odoo/requirements.txt'
+sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/custom'
+sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/custom/addons'
 
 # run it
 #cd /opt/odoo16/odoo

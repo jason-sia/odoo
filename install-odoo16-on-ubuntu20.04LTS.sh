8f0ed44b67ceccbdc71c5a9a8e9e219e5d78682d
Jason Sia <jasonbsia@gmail.com>
2024-05-23 09:47:17 +0800

    Update install-odoo16-on-ubuntu20.04LTS.sh

--- install-odoo16-on-ubuntu20.04LTS.sh
+++ install-odoo16-on-ubuntu20.04LTS.sh
@@ -64,10 +64,10 @@ sudo runuser -l odoo16 -c 'source /opt/odoo16/odoo-venv/bin/activate'
 sudo runuser -l odoo16 -c 'pip3 install wheel'
 sudo runuser -l odoo16 -c 'cd /'
 sudo runuser -l odoo16 -c 'pip3 install -r /opt/odoo16/odoo/requirements.txt'
-sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/log'
-sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/custom'
-sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/custom/addons'
-sudo runuser -l odoo16 -c 'wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo-server.conf -P /opt/odoo16/odoo-venv'
+#sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/log'
+#sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/custom'
+#sudo runuser -l odoo16 -c 'mkdir /opt/odoo16/odoo-venv/custom/addons'
+sudo runuser -l odoo16 -c 'wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo16.conf -P /etc'
  
 
 # run it

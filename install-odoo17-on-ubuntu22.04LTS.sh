98d04eb1cd558196c9251b79c618b1b0ce5a4c2b
Jason Sia <jasonbsia@gmail.com>
2024-05-24 15:37:14 +0800

    Update install-odoo17-on-ubuntu22.04LTS.sh

--- install-odoo17-on-ubuntu22.04LTS.sh
+++ install-odoo17-on-ubuntu22.04LTS.sh
@@ -8,6 +8,8 @@ sudo apt install -y postgresql
 wget -O - https://nightly.odoo.com/odoo.key | sudo apt-key add -
 echo "deb http://nightly.odoo.com/17.0/nightly/deb/ ./" | sudo tee /etc/apt/sources.list.d/odoo.list
 sudo apt update
+sudo apt upgrade -y
+
 
 #install odoo
 sudo apt install -y odoo

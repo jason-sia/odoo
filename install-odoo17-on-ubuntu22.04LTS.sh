018c07c80b3b1787f9a70a33b3993935d3f71573
Jason Sia <jasonbsia@gmail.com>
2024-05-24 19:09:40 +0800

    Update install-odoo17-on-ubuntu22.04LTS.sh

--- install-odoo17-on-ubuntu22.04LTS.sh
+++ install-odoo17-on-ubuntu22.04LTS.sh
@@ -11,6 +11,12 @@ echo "deb http://nightly.odoo.com/17.0/nightly/deb/ ./" | sudo tee /etc/apt/sour
 sudo apt update
 sudo apt upgrade -y
 
+#install Python 3.1
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install -y python3.10 python3.10-venv python3.10-dev
 
 #install odoo
 sudo apt install -y odoo

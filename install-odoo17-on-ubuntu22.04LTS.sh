482e5d48296219e6d5aee7a48e5d7068332b90d0
Jason Sia <jasonbsia@gmail.com>
2024-06-07 20:25:14 +0800

    Update install-odoo17-on-ubuntu22.04LTS.sh

--- install-odoo17-on-ubuntu22.04LTS.sh
+++ install-odoo17-on-ubuntu22.04LTS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # installing odoo env 17 dev
-sudo apt update
-sudo apt upgrade -y
+#sudo apt update
+#sudo apt upgrade -y
 
 git clone https://github.com/odoo/odoo.git
 
@@ -25,7 +25,7 @@ sudo chmod 700 /home/ubuntu/.ssh
 #sudo useradd -m -d /opt/odoo17 -U -r -s /bin/bash odoo17
 #sudo su - postgres -c "createuser -s odoo17"
 
-#createdb $USER
+createdb $USER
 #createdb odoo17
 
 cd odoo
@@ -35,7 +35,7 @@ sudo /home/ubuntu/odoo/setup/debinstall.sh
 #python3 odoo-bin --addons-path=addons -d odoo -i base
 
 # setup odoo config 
-sudo wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo17.conf -P /home/ubuntu/odoo
+#sudo wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo17.conf -P /home/ubuntu/odoo
 
 
 # create odoo service /etc/systemd/system/odoo.service
@@ -51,20 +51,21 @@ sudo wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo17.conf -P /
 #setup nginx
 #sudo apt-get install python-pypdf2 -y
 #sudo apt-get install python3-pypdf2 -y
-sudo apt install nginx -y 
-sudo rm -rf /etc/nginx/sites-available/default
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo.conf -P /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/odoo.conf /etc/nginx/sites-enabled/odoo.conf
-sudo nginx -t
 
-sudo mkdir /home/ubuntu/odoo/odoo-custom-addons
+#sudo apt install nginx -y 
+#sudo rm -rf /etc/nginx/sites-available/default
+#sudo rm -rf /etc/nginx/sites-enabled/default
+###sudo wget https://raw.githubusercontent.com/jason-sia/odoo/main/odoo.conf -P /etc/nginx/sites-available
+#sudo ln -s /etc/nginx/sites-available/odoo.conf /etc/nginx/sites-enabled/odoo.conf
+#sudo nginx -t
+
+#sudo mkdir /home/ubuntu/odoo/odoo-custom-addons
 
 # run the servers
-sudo service nginx stop
-sudo service nginx start
-#sudo systemctl start odoo
-#sudo systemctl status odoo
+#sudo service nginx stop
+#sudo service nginx start
+##sudo systemctl start odoo
+##sudo systemctl status odoo
 
 python3 /home/ubuntu/odoo/odoo-bin --addons-path=/home/ubuntu/odoo/addons -d odoo -i base
 #sudo /home/ubuntu/odoo/odoo-bin  -c /home/ubuntu/odoo/odoo17.conf -d odoo_db
